c597fe144e815cf61d1297afce541cdb886eeda1
Hayun <mycolki@gmail.com>
2021-07-26 15:57:21 +0900

    Feat: Add timer on Header.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import moment from 'moment';
+
+import { TIME_FORMAT } from '../../constants/timeText';
 
 export default function Header() {
+  const [clock, setClock] = useState(moment().format(TIME_FORMAT));
+
+  useEffect(() => {
+    const clockId = setInterval(() => {
+      setClock(moment().format(TIME_FORMAT));
+    }, 1000);
+
+    return () => clearInterval(clockId);
+  }, []);
+
   return (
     <Container>
-      <h1>Vanilla Clock</h1>
-      <h2>현재시간</h2>
+      <h1 className="app-title">Colki Alarm</h1>
+      <h2 className="current-time">{clock}</h2>
     </Container>
   )
 }
 
 const Container = styled.div`
   text-align: center;
-  background-color: tomato;
+
+  .app-title {
+    margin: 0;
+    padding: 10px;
+  }
+
+  .current-time {
+    margin: 0;
+    padding-bottom: 10px;
+    font-size: 20px;
+  }
 `;
