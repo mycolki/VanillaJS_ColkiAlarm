00b92060a1955b77f3cade6b777bcefcef09d9c3
Hayun <mycolki@gmail.com>
2021-07-30 08:52:27 +0900

    Design: Add body background color

--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import { createGlobalStyle } from 'styled-components';
 import store from './app/store';
 import App from './app/App';
 
+import { MAIN_BLUE_COLOR } from './constants/cssStyle';
+
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
+    background-color: ${MAIN_BLUE_COLOR};
   }
 `;
 
